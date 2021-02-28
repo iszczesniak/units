1fd6eecc2ca836d389993c1fab95a293192d7f75
Irek Szcześniak <irek.szczesniak@gmail.com>
2021-02-28 11:46:23 +0100

    units

--- test/cunits.cc
+++ test/cunits.cc
@@ -3,7 +3,23 @@
 void
 test1()
 {
-  assert(false);
+  CU cu13(1, 4);
+  CU cu24(2, 5);
+  CU cu23(2, 4);
+  CU cu14(1, 5);
+  CU cu12(1, 3);
+
+  assert(cu13 < cu24);
+  assert(cu13 < cu23);
+  assert(cu14 < cu23);
+
+  assert(cu12 < cu13);
+  assert(!(cu12 < cu12));
+  assert(!(cu13 < cu12));
+
+  assert(!(cu23 < cu14));
+  assert(!(cu23 < cu13));
+  assert(!(cu24 < cu13));
 }
 
 int
