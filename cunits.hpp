5ce5cb6c54d9f4b9cccc471fc641bfeeefcae1d4
Irek Szcześniak <irek.szczesniak@gmail.com>
2022-03-17 14:11:02 +0100

    made <=> nonmember

--- cunits.hpp
+++ cunits.hpp
@@ -53,78 +53,79 @@ public:
   {
     return !empty();
   }
-
-  // This operator establishes strict weak ordering needed for sorting
-  // in, e.g., containers.  We cannot use the inclusion relation,
-  // because it does not induce strick weak ordering as the
-  // incomparability of intervals is not transitive.
-  //
-  // We have to assure that if i properly includes j, then i < j.
-  //
-  // If an interval is not properly included in another interval, then
-  // these intervals are incomparable according to the inclusion
-  // relation.  We need to establish strict weak ordering between the
-  // incomparable intervals, and we do this by comparing the lower
-  // endpoints of the intervals.
-  //
-  // These are the possibilities of comparing i < j:
-  //
-  // 1. If min(i) < min(j): always i < j, regardless of:
-  //
-  //   a. max(i) < max(j): i and j are incomparable, but we say i < j
-  //                       because we need to establish a weak strict
-  //                       ordering, and the ordering between
-  //                       incomparable labels can be arbitrary
-  //
-  //   b. max(i) == max(j): i < j because i properly includes j
-  //
-  //   c. max(i) > max(j): i < j because i properly includes j
-  //
-  // 2. If min(i) == min(j), and:
-  //
-  //   a. max(i) < max(j): j properly includes i, and so j < i
-  //
-  //   b. max(i) == max(j): i == j, and so !(i < j)
-  //
-  //   c. max(i) > max(j): j < i, same as 2a, just swap i and j
-  //
-  // 3. If min(i) > min(j), and:
-  //
-  //   a. max(i) < max(j): same as 1c, just swap i and j
-  //
-  //   b. max(i) == max(j): same as 1b, just swap i and j
-  //
-  //   c. max(i) > max(j): same as 1a, just swap i and j
-  //
-  // This relation is transitive.
-  //
-  // This relation does not induce the lexicographic ordering.  It
-  // would, had we compared the upper endpoints with <, but then i < j
-  // would not hold (while it should) when i includes j.
-  //
-  // i < j if:
-  //
-  //   i.m_min < j.m_min || i.m_min == j.m_min && i.m_max > j.m_max
-  //
-  // i > j if:
-  //
-  //   i.m_min > j.m_min || i.m_min == j.m_min && i.m_max < j.m_max
-
-  constexpr std::strong_ordering
-  operator <=> (const cunits &a) const
-  {
-    if (m_min < a.m_min)
-      return std::strong_ordering::less;
-    if (m_min > a.m_min)
-      return std::strong_ordering::greater;
-    if (m_max > a.m_max)
-      return std::strong_ordering::less;
-    if (m_max < a.m_max)
-      return std::strong_ordering::greater;
-
-    return std::strong_ordering::equal;
-  }
 };
+
+// This operator establishes strict weak ordering needed for sorting
+// in, e.g., containers.  We cannot use the inclusion relation,
+// because it does not induce strick weak ordering as the
+// incomparability of intervals is not transitive.
+//
+// We have to assure that if i properly includes j, then i < j.
+//
+// If an interval is not properly included in another interval, then
+// these intervals are incomparable according to the inclusion
+// relation.  We need to establish strict weak ordering between the
+// incomparable intervals, and we do this by comparing the lower
+// endpoints of the intervals.
+//
+// These are the possibilities of comparing i < j:
+//
+// 1. If min(i) < min(j): always i < j, regardless of:
+//
+//   a. max(i) < max(j): i and j are incomparable, but we say i < j
+//                       because we need to establish a weak strict
+//                       ordering, and the ordering between
+//                       incomparable labels can be arbitrary
+//
+//   b. max(i) == max(j): i < j because i properly includes j
+//
+//   c. max(i) > max(j): i < j because i properly includes j
+//
+// 2. If min(i) == min(j), and:
+//
+//   a. max(i) < max(j): j properly includes i, and so j < i
+//
+//   b. max(i) == max(j): i == j, and so !(i < j)
+//
+//   c. max(i) > max(j): j < i, same as 2a, just swap i and j
+//
+// 3. If min(i) > min(j), and:
+//
+//   a. max(i) < max(j): same as 1c, just swap i and j
+//
+//   b. max(i) == max(j): same as 1b, just swap i and j
+//
+//   c. max(i) > max(j): same as 1a, just swap i and j
+//
+// This relation is transitive.
+//
+// This relation does not induce the lexicographic ordering.  It
+// would, had we compared the upper endpoints with <, but then i < j
+// would not hold (while it should) when i includes j.
+//
+// i < j if:
+//
+//   i.m_min < j.m_min || i.m_min == j.m_min && i.m_max > j.m_max
+//
+// i > j if:
+//
+//   i.m_min > j.m_min || i.m_min == j.m_min && i.m_max < j.m_max
+
+template<typename T>
+constexpr std::strong_ordering
+operator <=> (const cunits<T> &i, const cunits<T> &j)
+{
+  if (i.min() < j.min())
+    return std::strong_ordering::less;
+  if (i.min() > j.min())
+    return std::strong_ordering::greater;
+  if (i.max() > j.max())
+    return std::strong_ordering::less;
+  if (i.max() < j.max())
+    return std::strong_ordering::greater;
+
+  return std::strong_ordering::equal;
+}
 
 template<typename T>
 bool
