1a9dd04651d961ad53c03ba0f5e351191d4aa689
Irek Szcześniak <irek.szczesniak@gmail.com>
2023-11-17 13:59:20 +0100

    tests

--- test/sunits.cc
+++ test/sunits.cc
@@ -9,8 +9,27 @@ test1()
   assert(s.size() == 6);
 }
 
+void
+test_includes_cu()
+{
+  SU s0{};
+  assert(!includes(s0, CU(0, 1)));  
+
+  SU s1{{10, 20}};
+  assert(includes(s1, CU(10, 11)));
+  assert(includes(s1, CU(14, 16)));
+  assert(includes(s1, CU(19, 20)));
+  assert(includes(s1, CU(10, 20)));
+
+  assert(!includes(s1, CU(5, 15)));
+  assert(!includes(s1, CU(9, 10)));
+  assert(!includes(s1, CU(20, 21)));
+  assert(!includes(s1, CU(15, 25)));
+}
+
 int
 main()
 {
   test1();
+  test_includes_cu();
 }
