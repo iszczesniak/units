ae119b306442ce77267538eb54408d6ed850dc4c
Irek Szcześniak <irek.szczesniak@gmail.com>
2024-04-11 09:31:58 +0200

    helpers

--- test/helpers.hpp
+++ test/helpers.hpp
@@ -1,12 +1,14 @@
 #ifndef HELPERS_HPP
 #define HELPERS_HPP
 
+#include <compare>
+
 // Make sure that i < j.
 template <typename T>
 bool
 is_less(const T &i, const T &j)
 {
-  bool status = true;
+  bool status = ((i <=> j) == std::strong_ordering::less);
 
   status &= (i < j);
   status &= (i <= j);
@@ -23,7 +25,7 @@ template <typename T>
 bool
 is_equal(const T &i, const T &j)
 {
-  bool status = true;
+  bool status = ((i <=> j) == std::strong_ordering::equal);
 
   status &= (i == j);
   status &= (!(i != j));
