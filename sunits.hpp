55501269e61c7d6f20c1eba203b7a175751f025f
iszczesniak <irek.szczesniak@gmail.com>
2024-05-26 09:46:53 +0200

    comments

--- sunits.hpp
+++ sunits.hpp
@@ -11,41 +11,26 @@
 #include <numeric>
 #include <vector>
 
-// sunits is the set of cunits.  Non-overlapping cunits are stored in
-// the base vector that we keep sorted.  Not every cu can be inserted
-// or removed: we can insert only a cunit that in no part is already
-// included, we can remove only a cunit that is already included.  We
-// do not allow for inserting or removing any cunits, becasue we do
-// not need it, and the implementation would be more complicated and
-// less efficient.
+// A sequence of non-overlapping intervals.  Intervals are stored in a
+// base container that we keep sorted.  Since the intervals do not
+// overlap (and so do not include one another), then < establishes
+// linear ordering.  It is enough to compare the intervals by the
+// lower endpoints only (and that is covered by the cases 1a and 3c,
+// and implemented by <=>).
 //
-// We need to find the first cunits that is greater than a given cu.
-// Function upper_bound(begin(), end(), cu) returns iterator i to the
+// To keep the implementation simple and efficient, we offer only the
+// minimal needed functionality:
+//
+// * insert only an interval that in no part is already included,
+//
+// * remove only an interval that is already included.
+//
+// To insert or remove an interval iv, we need to find the first
+// interval in the sequence that is greater than iv (that follows iv).
+// Function upper_bound(begin(), end(), iv) returns iterator i to the
 // first (as the iterator gets incremented, i.e., from left to right)
-// cunits object in the base container such that cu < *i.
-//
-// Function std::upper_bound(begin(), end(), cu) may return a pointer
-// to the beginning or the end.  If there is a cunits p that is on the
-// left of *i (i.e., *(i - 1) exists), relation p < cu must hold
-// because:
-//
-// * element *i is the first for which cu < *i holds,
-//
-// * elements in the base container are sorted with <,
-//
-// * elements in the base contatiner are non-overlapping,
-//
-// * ordering < is transitive and total.
-//
-// As given in the table in cunits.hpp, relation p < cu holds when p
-// || cu or p properly includes cu.
-//
-// The above p || cu of p < cu happens when min(p) < min(cu) and
-// max(p) < max(cu) in two cases:
-//
-// * p precedes cu, i.e., p.max() <= cu.min(),
-//
-// * p overlaps with cu.
+// interval in the base container such that iv < *i.  The function may
+// return a pointer to the beginning or the end.
 
 template <typename T>
 struct sunits: private std::vector<cunits<T>>
@@ -80,27 +65,44 @@ struct sunits: private std::vector<cunits<T>>
                            {return c + in.size();});
   }
 
-  // Insert a CU.  No part of the cu can already be included.
+  // Insert a new interval.  No part of it can already be included.
   void
   insert(const data_type &iv)
   {
-    // Returns a position where to insert the cu.  Since the cunits
-    // (the cu and those in the base container) do not overlap with
-    // and do not include other cunits, then p <= cu < *i means that p
-    // precedes cu (p and cu cannot equal) and cu precedes *i.
+    // Returns a position i where to insert iv.
+    //
+    // Returned i is such that iv < *i, and since the intervals (iv
+    // and in the container) do not overlap, then we put iv just
+    // before *i.  If there is an interval p in the container that
+    // preceds *i, then iv should be placed afert p because p < iv
+    // holds:
+    //
+    // * iv < p is false (because *i is the first from the left such
+    //   that iv < *i), so iv >= p holds (because < is total),
+    //
+    // * the intervals are totally ordered as they do not overlap,
+    //
+    // * the intervals in the container are sorted with <.
+    //
+    // That's how it looks on the axis:
+    //
+    //      p           iv       *i
+    // |----*======o----*===o----*======o------->
     auto i = std::upper_bound(begin(), end(), iv,
                               std::greater<data_type>());
     auto j = i;
 
-    // These are the endpoints of the new CU.  Look left and right for
-    // neighboring CUs to remove and then to merge into the new CU.
+    // These are the endpoints of the interval to insert.  Look left
+    // and right for neighboring intervals to remove and then to merge
+    // into the new inerval.
     auto min = iv.min();
     auto max = iv.max();
 
-    // Left.  We need variable i2, because we need to decrement the
-    // iterator.  Instead of (--i2), we could do (i2 - 1), but that
-    // would require a random access iterator.  With --i2, we require
-    // only bidirectional iterator.
+    // Look left.  By one position only, hence just "if", and not
+    // "while".  We need variable i2, because we need to decrement the
+    // iterator to look left.  Instead of copying i and (--i2), we
+    // could do (i - 1), but that would require a random access
+    // iterator.  With --i2, we require only a bidirectional iterator.
     if (i != begin())
       if (auto i2 = i; (--i2)->max() == min)
         --i, min = i->min();
@@ -118,15 +120,44 @@ struct sunits: private std::vector<cunits<T>>
     assert(verify());
   }
 
-  // Remove a CU.  The cu must be already included.
+  // Remove an interval iv.  The interval must be already included.
+  //
+  // The container must have at least one interval p:
+  //
+  // |---*=======o------->
+  //
+  // These are the possible intervals iv:
+  //
+  //     *===o       p < vi
+  //     *=======*   p == vi
+  //         *===o   p < vi
+  //      *=====*    p < vi
+  //
+  // Since p must include or equal vi, then p <= iv.
+  //
+  // Here's the plan:
+  //
+  // * find an interval pointed with i such that iv < *i,
+  //
+  // * there must be a interval p that precedes *i,
+  //
+  // * interval p includes iv, so work with p.
   void
   remove(const data_type &iv)
   {
-    // Iterator i points to the first element for which cu < i*.
+    // Iterator i points to the first element for which iv < *i.
     auto i = std::upper_bound(begin(), end(), iv,
                               std::greater<data_type>());
 
-    // There must exist element p previous to *i such that p <= cu.
+    // There must exist an element p previous to *i such that p <= iv.
+    //
+    // We know that:
+    //
+    // * p <= iv - since p includes iv,
+    //
+    // * interval *i is the first from left for which iv < *i.
+    //
+    // And so: p <= iv < *i
     assert(i != begin());
     --i;
 
@@ -137,10 +168,11 @@ struct sunits: private std::vector<cunits<T>>
     i = base_type::erase(i);
 
     // If there were some units on the right in p, we add them.  We
-    // first insert the CU from the right side to get iterator i where
-    // we insert the CU from the left side.  We can't swap the order
-    // (i.e., first from the left, then from the right) because the
-    // CUs would be reversed, making the base container inconsistent.
+    // first insert the leftover interval from the right side to get
+    // iterator i where we insert the leftover interval from the left
+    // side.  We can't swap the order (i.e., first from the left, then
+    // from the right) because the leftover intervals would be
+    // reversed, making the base container inconsistent.
     if (iv.max() < cop.max())
       i = base_type::insert(i, data_type(iv.max(), cop.max()));
     // If there were some units on the left in p, we add them.
@@ -151,7 +183,7 @@ struct sunits: private std::vector<cunits<T>>
   }
 
 private:
-  // Make sure the objects are in the order.
+  // Make sure the intervals are in order.
   bool
   verify()
   {
@@ -175,10 +207,11 @@ private:
 template <typename T>
 auto operator <=> (const sunits<T> &i, const sunits<T> &j)
 {
-  // Should be as easy as below, but not accepted by older compilers.
+  // Could be as easy as below, but ain't accepted by older compilers.
   //
   // return std::lexicographical_compare_three_way(i.begin(), i.end(),
-  //                                               j.begin(), j.end());
+  //                                               j.begin(),
+  //                                               j.end());
 
   auto ii = i.begin();
   auto ji = j.begin();
@@ -241,13 +274,14 @@ operator >> (std::istream &in, sunits<T> &su)
   return in;
 }
 
+// Every interval of b, has to be in a.
 template <typename T>
 bool
 includes(const sunits<T> &a, const sunits<T> &b)
 {
   auto i = a.begin();
 
-  // Every cu of a, has to be in *this.
+  // Labels in a and b are sorted.
   for(const auto &iv: b)
     while(true)
       {
@@ -307,7 +341,7 @@ intersection(const sunits<T> &a, const sunits<T> &b)
           continue;
         }
 
-      // At this point the CU's of i and j overlap.
+      // At this point the intervals of i and j overlap.
       auto min = std::max(i->min(), j->min());
       auto max = std::min(i->max(), j->max());
       ret.insert({min, max});
